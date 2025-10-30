7408f1e769c1fec23656604840897f1f781379a4
Jimmy <jimmy.kang@igaworks.com>
2025-10-30 11:30:08 +0900

    sample

--- example/ios/Runner/AppDelegate.swift
+++ example/ios/Runner/AppDelegate.swift
@@ -8,6 +8,10 @@ import UIKit
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+      
+    //MARK: Push Setting
+    UNUserNotificationCenter.current().delegate = self
+      
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
